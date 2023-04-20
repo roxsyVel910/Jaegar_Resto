cf368c6fc926b73f48ea9af05f59af5b1147a3b0
Roxsy <roxsy.dj@gmail.com>
2023-04-19 23:58:55 -0500

    configuracion entorno de trabajo e instalacion de librerias

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-red-400">
+    hola mundo
+      
+     
+    </div>
+  )
+}
+
+export default App
